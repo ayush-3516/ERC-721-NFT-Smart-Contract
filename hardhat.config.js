22c5823ced7c3192b4256f53495a0e680c13aec5
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-08-03 14:59:34 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
